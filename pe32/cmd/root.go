787c94b4378076489708145b26ed97adc5430cab
0918nobita <nobita.0918@gmail.com>
2020-05-19 16:11:35 +0900

    :sparkles: for 文を用いたサンプルを追加

--- pe32/cmd/root.go
+++ pe32/cmd/root.go
@@ -10,12 +10,22 @@ import (
 // Execute main procedures
 func Execute(version string, revision string) {
 	fmt.Println("PE32 Parser", version, "rev", revision)
+
 	rand.Seed(time.Now().UnixNano())
 	fmt.Println("My favorite number is", rand.Intn(10))
+
 	fmt.Println(math.Pi)
+
 	fmt.Println("3 + 4 =", add(3, 4))
-	a, b := swap("world", "hello")
+
+	var a, b = swap("world", "hello")
 	fmt.Println(a, b)
+
+	var sum = 0;
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println("Sum:", sum)
 }
 
 func add(x, y int) int {
