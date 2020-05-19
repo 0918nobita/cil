186ad382ec67a6a2837a015ab5aa0d0d1b353f94
0918nobita <nobita.0918@gmail.com>
2020-05-19 15:09:27 +0900

    :sparkles: swap 関数を追加

--- pe32/cmd/root.go
+++ pe32/cmd/root.go
@@ -14,8 +14,14 @@ func Execute(version string, revision string) {
 	fmt.Println("My favorite number is", rand.Intn(10))
 	fmt.Println(math.Pi)
 	fmt.Println("3 + 4 =", add(3, 4))
+	a, b := swap("world", "hello")
+	fmt.Println(a, b)
 }
 
 func add(x, y int) int {
 	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
 }
